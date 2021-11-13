85989daab2b21703cfa2b822ccf2f91dad4aa6c0
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-13 21:02:12 +0200

    post alt text and spinner created

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,29 @@
   <div id="app">
     <div class="container">
       <div class="posts">
+        <!-- Posts -->
         <h1>Posts</h1>
-        <div class="post" v-for="(post, index) in allPosts" :key="index">
-          <h2>{{ post.title }}</h2>
-          <p>{{ post.body }}</p>
+        <div v-if="allPosts.length">
+          <div
+            class="post"
+            v-for="(post, index) in allPosts"
+            :key="index"
+            v-bind:class="{ ['post--alone']: allPosts.length === 1 }"
+          >
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.body }}</p>
+          </div>
         </div>
+
+        <!-- Spinner -->
+        <div class="text-center" v-if="!isLoaded">
+          <img width="100px" src="./assets/spinner.svg" alt="spinner" />
+        </div>
+
+        <!-- Alt alert  -->
+        <p class="text-center" v-if="!allPosts.length && isLoaded">
+          No posts yet
+        </p>
       </div>
     </div>
   </div>
@@ -16,21 +34,27 @@
 export default {
   name: "App",
   computed: {
-    allPosts(){
-      return this.$store.getters.allPosts
-    }
+    allPosts() {
+      return this.$store.getters.allPosts;
+    },
   },
   data() {
     return {
       posts: [],
+      isLoaded: false,
     };
+  },
+  methods: {
+    sleep: (time = 1000) => new Promise((r) => setTimeout(r, time)),
   },
   async mounted() {
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/posts?_limit=10"
     );
     const posts = await res.json();
+    await this.sleep();
     this.posts = posts;
+    this.isLoaded = true;
   },
 };
 </script>
@@ -41,6 +65,7 @@ export default {
 ::before {
   box-sizing: border-box;
 }
+
 body,
 html {
   width: 100%;
@@ -50,6 +75,7 @@ html {
   margin: 0;
   padding: 0;
 }
+
 #app {
   width: 100%;
   height: 100vh;
@@ -69,12 +95,12 @@ html {
   border: 1px solid #efefef;
   border-top: 0;
   padding: 1rem;
-  
+
   & > h2 {
     text-transform: capitalize;
   }
 
-  &:nth-child(2) {
+  &:first-child {
     border-radius: 5px 5px 0 0;
     border-top: 1px solid #efefef;
   }
@@ -83,5 +109,15 @@ html {
     border-radius: 0 0 5px 5px;
     border-top: 0;
   }
+
+  &.post--alone {
+    border: 1px solid #efefef;
+    border-radius: 5px;
+  }
 }
+
+.text-center {
+  text-align: center;
+}
+
 </style>
